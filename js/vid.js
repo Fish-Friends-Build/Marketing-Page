81c06eda84ac30957da3670bc57847c534e56d31
Jason Neale <40077498+JasonNeale@users.noreply.github.com>
2020-01-06 17:33:13 -0500

    Add files via upload

---
+++ js/vid.js
@@ -0,0 +1,12 @@
+let video = document.getElementById("myVideo");
+let btn = document.getElementById("myBtn");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
